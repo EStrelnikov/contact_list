0171ec51c4758bba2820b23cb7b3228ce48c73e3
Evgeny Strelnikov <estrelnikov1@yandex.ru>
2023-07-06 00:30:34 +0300

    redux

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,27 +1,23 @@
-import React, {FC, useEffect, useState} from 'react';
-import {CommonPageProps} from './types';
-import {Col, Row} from 'react-bootstrap';
-import {useParams} from 'react-router-dom';
-import {ContactDto} from 'src/types/dto/ContactDto';
-import {ContactCard} from 'src/components/ContactCard';
-import {Empty} from 'src/components/Empty';
+import { Col, Row } from 'react-bootstrap'
+import { Navigate, useParams } from 'react-router-dom'
+import { ContactCard } from 'src/components/ContactCard'
+import { Empty } from 'src/components/Empty'
+import { useAppSelector } from 'src/store'
+import { getContactById } from 'src/store/contacts'
 
+export const ContactPage = () => {
+  const { contactId } = useParams<{ contactId: string }>()
+  const contact = useAppSelector(getContactById(contactId || ''))
 
-export const ContactPage: FC<CommonPageProps> = ({
-  contactsState
-}) => {
-  const {contactId} = useParams<{ contactId: string }>();
-  const [contact, setContact] = useState<ContactDto>();
-
-  useEffect(() => {
-    setContact(() => contactsState[0].find(({id}) => id === contactId));
-  }, [contactId]);
+  if (!contact) {
+    return <Navigate to='/contact' />
+  }
 
   return (
-    <Row xxl={3}>
-      <Col className={'mx-auto'}>
-        {contact ? <ContactCard contact={contact} /> : <Empty />}
-      </Col>
-    </Row>
-  );
-};
+      <Row xxl={3}>
+        <Col className={'mx-auto'}>
+          {contact ? <ContactCard contact={contact} /> : <Empty />}
+        </Col>
+      </Row>
+  )
+}
